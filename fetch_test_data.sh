fa6d45cba3b71ec928acb102b55801902145c581
George Webb <3473169+webbgeorge@users.noreply.github.com>
2025-02-02 16:51:37 +0000

    run tests on more top podcasts

--- fetch_test_data.sh
+++ fetch_test_data.sh
@@ -16,9 +16,17 @@ function processChartURL() {
 }
 
 function downloadTestDataFeed() {
+  if [[ $2 == "null" ]]; then
+    echo "no feed url, skipping"
+    return
+  fi
   curl -sL -o "testdata/top-podcasts/${1}.xml" $2
 }
 
 mkdir -p testdata/top-podcasts
 
+# use the top 50 UK, top 50 US, top 10 German and top 10 Japanese podcasts for tests
 processChartURL "https://rss.marketingtools.apple.com/api/v2/gb/podcasts/top/50/podcasts.json"
+processChartURL "https://rss.marketingtools.apple.com/api/v2/us/podcasts/top/50/podcasts.json"
+processChartURL "https://rss.marketingtools.apple.com/api/v2/de/podcasts/top/10/podcasts.json"
+processChartURL "https://rss.marketingtools.apple.com/api/v2/jp/podcasts/top/10/podcasts.json"
